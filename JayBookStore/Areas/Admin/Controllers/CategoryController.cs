35a6b8454107b2c8c4d4cb2d3a66396dac1b60ce
Jay Patel <91098462+JayPatel092001@users.noreply.github.com>
2021-11-15 11:20:56 -0500

    Add Another files

---
+++ JayBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using JayBooks.DataAccess.Repository.IRepository;
+using JayBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JayBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if(id == null)
+            {
+                return View(category);
+            }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+
+}

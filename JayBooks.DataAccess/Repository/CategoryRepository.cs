cbb5abf232bf9992439454d1d734bd8f6e5b827b
Jay Patel <91098462+JayPatel092001@users.noreply.github.com>
2021-11-15 02:05:45 -0500

    Change repository

---
+++ JayBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JayBooks.DataAccess.Repository.IRepository;
+using JayBooks.Models;
+using JayBookStore.DataAccess.Data;
+
+
+namespace JayBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+
+    internal interface ICategoryRepository
+    {
+    }
+}
---
+++ JayBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using JayBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JayBooks.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        void Update(Category category);
+    }
+}
